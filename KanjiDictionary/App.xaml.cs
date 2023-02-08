a8f0e1853fcd7eda749bae2dbb6435c51032824e
Caleb Lugo <cnl1211999@gmail.com>
2023-02-07 17:53:31 -0800

    add empty maui project

---
+++ KanjiDictionary/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace KanjiDictionary;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ KanjiDictionary/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace KanjiDictionary;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
